ad431d5ac4adc01da6a9846118525c9a9ccdd14c
afnan qasim <96981692+afnanqasim74@users.noreply.github.com>
2023-07-11 22:35:20 +0500

    app.py

--- app.py
+++ app.py
@@ -1,82 +1,63 @@
-
-""""""
-from langchain.chains.question_answering import load_qa_chain
-from langchain import HuggingFaceHub
-import os
-
 import streamlit as st
-# Embeddings
+from langchain.chains.question_answering import load_qa_chain
 from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores.faiss import FaissVectorStore
+from langchain.huggingfacehub import HuggingFaceHub
+import pickle
+import os
 import time
 
+# Set Hugging Face Hub API token
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model_path = "db.pkl"
+    with open(model_path, "rb") as file:
+        model = pickle.load(file)
+    return model
 
-
-
-import os
-
-@st.cache_resource
-def libraries():
-
-    os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
-
-
-    llm=HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":1, "max_length":512})
-
-
-    c = load_qa_chain(llm, chain_type="stuff")
-
-    embeddings = HuggingFaceEmbeddings()
-
-
-
-    os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
-    return c 
-
-c = libraries()
-
-# Text Splitter
-
-# Load the chain
-import pickle
-
-
-
-
-# Load the chain
-@st.cache_resource
-def model():
-
-    load_path = "db.pkl"
-    with open(load_path, "rb") as af:
-        d = pickle.load(af)
-        return d
-d  = model()
-
+@st.cache(allow_output_mutation=True)
+def load_chain():
+    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature": 1, "max_length": 512})
+    chain = load_qa_chain(llm, chain_type="stuff")
+    return chain
 
 def main():
-    st.title("CHAT LUB ")
+    st.title("CHAT LUB")
+
+    # Load model
+    model = load_model()
+
+    # Load chain
+    chain = load_chain()
 
     # User input
     input_text = st.text_input("Enter the starting sentence")
 
-    # Generate story when button is clicked
+    # Generate answer when button is clicked
     if st.button("ENTER"):
-        progress_text = "Generating answer...🤗"
+        progress_text = "Generating answer... 🤗"
         my_bar = st.progress(0)
         my_bar_text = st.empty()
 
         for percent_complete in range(100):
-            time.sleep(.01)
+            time.sleep(0.01)
             my_bar.progress(percent_complete + 1)
             my_bar_text.text(f"{progress_text} {percent_complete + 1}%")
-        docs = d.similarity_search(input_text)
-        a = c.run(input_documents=docs, question=input_text) 
-        generated_story = a
 
-        # Display the generated story
-        #st.write("Generated Story:")
-        st.text_area("Generated anwer", generated_story, height=200)
+        # Perform similarity search
+        vector_store = FaissVectorStore()
+        vector_store.load_from_path("path_to_vector_store")  # Replace with the actual path
+        vectors = vector_store.get_vectors()
+        k = 5  # The number of nearest neighbors to search for
+        docs, _ = vectors.similarity_search(input_text, k=k)
+
+        # Generate answer using the chain
+        generated_answer = chain.run(input_documents=docs, question=input_text)
+
+        # Display the generated answer
+        st.text_area("Generated answer", generated_answer, height=200)
 
 if __name__ == "__main__":
     main()
